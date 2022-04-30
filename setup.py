abc21127481ca748270f55fd4567609800ce674c
akam1o <kamioakira0422@gmail.com>
2022-05-01 01:24:41 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="stealchecker",
-    version="0.0.1",
+    version="1.0.0",
     entry_points={
         "console_scripts": [
             "stealcheck = stealchecker:stealchecker.main",
