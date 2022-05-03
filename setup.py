bf8ee0fc908e56e3b801aeb61857b94326d29dbd
akam1o <5158577+akam1o@users.noreply.github.com>
2022-05-03 23:33:54 +0900

    update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,45 @@
 from setuptools import setup
 
+DESCRIPTION = 'stealchecker: Checking CPU steal of VM from virtual host'
+NAME = 'stealchecker'
+AUTHOR = 'Akira KAMIO'
+AUTHOR_EMAIL = '5158577+akam1o@users.noreply.github.com'
+URL = 'https://github.com/akam1o/stealchecker'
+LICENSE = 'MIT'
+DOWNLOAD_URL = URL
+VERSION = '1.1.0'
+PYTHON_REQUIRES = '<3'
+INSTALL_REQUIRES = [
+    'libvirt-python<=5.10'
+]
+PACKAGES = [
+    'stealchecker'
+]
+KEYWORDS = 'libvirt kvm cpu steal'
+CLASSIFIERS=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 2 :: Only'
+]
+with open('README.md', 'r', encoding='utf-8') as fp:
+        readme = fp.read()
+LONG_DESCRIPTION = readme
+LONG_DESCRIPTION_CONTENT_TYPE = 'text/markdown'
+
 setup(
-    name="stealchecker",
-    version="1.0.0",
-    entry_points={
-        "console_scripts": [
-            "stealchecker = stealchecker:stealchecker.main",
-        ]
-    },
-    packages=["stealchecker"]
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
+    author=AUTHOR,
+    author_email=AUTHOR_EMAIL,
+    maintainer=AUTHOR,
+    maintainer_email=AUTHOR_EMAIL,
+    url=URL,
+    download_url=URL,
+    packages=PACKAGES,
+    classifiers=CLASSIFIERS,
+    license=LICENSE,
+    keywords=KEYWORDS,
+    install_requires=INSTALL_REQUIRES
 )
